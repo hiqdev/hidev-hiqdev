5c4bb80f76cd623e7857fe9ee551f492db3f53d5
Andrii Vasyliev <sol@hiqdev.com>
2016-01-06 09:51:51 +0000

    + yii2-extraconfig

---
+++ src/yii2-extraconfig.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * HiQDev vendor configuration for HiDev
+ *
+ * @link      https://github.com/hiqdev/hidev-vendor
+ * @package   hidev-vendor
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+return [
+    'configFiles' => [
+        '@hidev/hiqdev/config.yml',
+    ],
+    'views' => [
+        '@hidev/hiqdev/views',
+    ],
+];
