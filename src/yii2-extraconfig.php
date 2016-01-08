953dbe2f367b282ef8b9bfaafac13afed3405b0c
Andrii Vasyliev <sol@hiqdev.com>
2016-01-08 19:15:29 +0000

    Changed  ->

--- src/yii2-extraconfig.php
+++ src/yii2-extraconfig.php
@@ -12,7 +12,7 @@
 return [
     'components' => [
         'config' => [
-            'configs' => [
+            'include' => [
                 '@hidev/hiqdev/config.yml',
             ],
             'views' => [
